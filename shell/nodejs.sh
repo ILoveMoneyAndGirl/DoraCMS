6dd00118e0e18c3fc2cb4ab65cccae99858135c2
嗝屁 <1010017806@qq.com>
2019-06-26 16:49:37 +0800

    jsnd

--- shell/nodejs.sh
+++ shell/nodejs.sh
@@ -1,8 +1,6 @@
 apt-get update
 apt-get install python gcc make g++ wget
 wget https://nodejs.org/download/rc/v8.9.1-rc.1/node-v8.9.1-rc.1.tar.gz
-tar -zxvf node-v8.9.1-rc.1.tar.gz 
-cd node-v8.9.1-rc.1.tar.gz 
-./configure
+tar -zxvf /root/node-v8.9.1-rc.1.tar.gz 
+/root/node-v8.9.1-rc.1/configure
 make install
-cd ../
