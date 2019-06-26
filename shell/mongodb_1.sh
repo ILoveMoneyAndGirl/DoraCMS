4373653ce340d02da219400585d011d7b4247b16
嗝屁 <1010017806@qq.com>
2019-06-26 16:59:00 +0800

    dsd

--- shell/mongodb_1.sh
+++ shell/mongodb_1.sh
@@ -2,7 +2,7 @@ sudo apt-get  update
 #mongodb 所需库
 
 wget -P /root/ https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-4.0.10.tgz
-tar -zxvf /root/mongodb-linux-x86_64-ubuntu1604-4.0.10.tgz
+tar -zxvf /root/mongodb-linux-x86_64-ubuntu1604-4.0.10.tgz -C /root/
 mv /root/mongodb-linux-x86_64-ubuntu1604-4.0.10/ /usr/local/mongodb
 
 mkdir -p /usr/local/mongodb/data
--- shell/nodejs.sh
+++ shell/nodejs.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install python gcc make g++ wget
 wget -P /root/ https://nodejs.org/download/rc/v8.9.1-rc.1/node-v8.9.1-rc.1.tar.gz
-tar -zxvf /root/node-v8.9.1-rc.1.tar.gz 
+tar -zxvf /root/node-v8.9.1-rc.1.tar.gz -C /root/
 /root/node-v8.9.1-rc.1/configure
 make install
