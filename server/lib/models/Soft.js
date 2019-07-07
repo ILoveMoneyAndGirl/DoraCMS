9f2dff8272211ba68a347d12a5a1763bfd2a75e3
嗝屁 <1010017806@qq.com>
2019-07-07 16:43:50 +0800

    d

--- server/lib/models/Soft.js
+++ server/lib/models/Soft.js
@@ -14,7 +14,7 @@ var SoftSchema = new Schema({
     name: String,
     ip: String,//通信ip
     port: { type: Number, default: 8888 }, // 通信端口
-    comments: String // 描述
+    comments: String, // 描述
     type:{ type: Number, default: 0},//产品类型
 });
 
