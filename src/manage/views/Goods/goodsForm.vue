ff696946dcad897bb803c8a1b57ed03d18df2f4b
嗝屁 <1010017806@qq.com>
2019-07-08 16:04:03 +0800

    cc

--- src/manage/views/Goods/goodsForm.vue
+++ src/manage/views/Goods/goodsForm.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="dr-contentTagForm">
-        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('contentTag.form_title')" :visible.sync="dialogState.show" :close-on-click-modal="false">
+    <div class="dr-goodsForm">
+        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('goods.form_title')" :visible.sync="dialogState.show" :close-on-click-modal="false">
             <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
                 <el-form-item :label="$t('goods.price')" prop="price">
                     <el-input size="small" v-model="dialogState.formData.price"></el-input>
