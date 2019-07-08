cd3aa326efc5fe6b627b8c1b9cdb603a49f7027e
嗝屁 <1010017806@qq.com>
2019-07-08 16:21:59 +0800

    goodsForm

--- src/manage/views/Goods/goodsForm.vue
+++ src/manage/views/Goods/goodsForm.vue
@@ -90,11 +90,11 @@ export default {
           } else {
             // 新增
             services.AddGoods(params).then(result => {<template>
-    <div class="dr-contentTagForm">
+    <div class="dr-goodsForm">
         <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('goods.form_title')" :visible.sync="dialogState.show" :close-on-click-modal="false">
             <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
                 <el-form-item :label="$t('goods.price')" prop="price">
-                    <el-input size="small" v-model="dialogState.formData.name"></el-input>
+                    <el-input size="small" v-model="dialogState.formData.price"></el-input>
                 </el-form-item>
                 <el-form-item :label="$t('goods.days')" prop="days">
                     <el-input size="small" type="textarea" v-model="dialogState.formData.days"></el-input>
