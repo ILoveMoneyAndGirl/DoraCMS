731f8c9357320c130813b0b17e81fcb816b78da2
嗝屁 <1010017806@qq.com>
2019-07-08 17:01:06 +0800

    dfdf

--- src/manage/views/Goods/goodsForm.vue
+++ src/manage/views/Goods/goodsForm.vue
@@ -8,97 +8,6 @@
                 <el-form-item :label="$t('goods.days')" prop="days">
                     <el-input size="small" type="textarea" v-model="dialogState.formData.days"></el-input>
                 </el-form-item>
-                 <el-form-item :label="$t('goods.des')" prop="des">
-                    <el-input size="small" type="textarea" v-model="dialogState.formData.des"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button size="medium" type="primary" @click="submitForm('ruleForm')">{{dialogState.edit ? $t('main.form_btnText_update') : $t('main.form_btnText_save')}}</el-button>
-                </el-form-item>
-            </el-form>
-        </el-dialog>
-    </div>
-</template>
-<script>
-import services from "../../store/services.js";
-import _ from "lodash";
-export default {
-  props: {
-    dialogState: Object,
-    groups: Array
-  },
-  data() {
-    return {
-      rules: {
-        name: [
-          {
-            required: true,
-            message: this.$t("validate.inputNull", {
-              label: this.$t("contentTag.name")
-            }),
-            trigger: "blur"
-          },
-          {
-            min: 1,
-            max: 12,
-            message: this.$t("validate.rangelength", { min: 1, max: 12 }),
-            trigger: "blur"
-          }
-        ],
-        comments: [
-          {
-            required: true,
-            message: this.$t("validate.inputNull", {
-              label: this.$t("main.comments_label")
-            }),
-            trigger: "blur"
-          },
-          {
-            min: 2,
-            max: 30,
-            message: this.$t("validate.ranglengthandnormal", {
-              min: 2,
-              max: 30
-            }),
-            trigger: "blur"
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    confirm() {
-      this.$store.dispatch("hideGoodsForm");
-    },
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          let params = this.dialogState.formData;
-          // 更新
-          if (this.dialogState.edit) {
-            services.updateGoods(params).then(result => {
-              if (result.data.status === 200) {
-                this.$store.dispatch("hideGoodsForm");
-                this.$store.dispatch("getGoodsList");
-                this.$message({
-                  message: this.$t("main.updateSuccess"),
-                  type: "success"
-                });
-              } else {
-                this.$message.error(result.data.message);
-              }
-            });
-          } else {
-            // 新增
-            services.AddGoods(params).then(result => {<template>
-    <div class="dr-goodsForm">
-        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="填写信息" :visible.sync="true" :close-on-click-modal="false">
-            <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-                <el-form-item :label="$t('goods.price')" prop="price">
-                    <el-input size="small" v-model="dialogState.formData.price"></el-input>
-                </el-form-item>
-                <el-form-item :label="$t('goods.days')" prop="days">
-                    <el-input size="small" type="textarea" v-model="dialogState.formData.days"></el-input>
-                </el-form-item>
                 <el-form-item>
                     <el-button size="medium" type="primary" @click="submitForm('ruleForm')">{{dialogState.edit ? $t('main.form_btnText_update') : $t('main.form_btnText_save')}}</el-button>
                 </el-form-item>
@@ -163,7 +72,7 @@ export default {
           let params = this.dialogState.formData;
           // 更新
           if (this.dialogState.edit) {
-            services.updateGoods(params).then(result => {
+            services.updateContentTag(params).then(result => {
               if (result.data.status === 200) {
                 this.$store.dispatch("hideGoodsForm");
                 this.$store.dispatch("getGoodsList");
@@ -177,31 +86,10 @@ export default {
             });
           } else {
             // 新增
-            services.AddGoods(params).then(result => {
+            services.addContentTag(params).then(result => {
               if (result.data.status === 200) {
                 this.$store.dispatch("hideGoodsForm");
                 this.$store.dispatch("getGoodsList");
-                this.$message({
-                  message: this.$t("main.addSuccess"),
-                  type: "success"
-                });
-              } else {
-                this.$message.error(result.data.message);
-              }
-            });
-          }
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    }
-  }
-};
-</script>
-              if (result.data.status === 200) {
-                this.$store.dispatch("hideContentTagForm");
-                this.$store.dispatch("getContentTagList");
                 this.$message({
                   message: this.$t("main.addSuccess"),
                   type: "success"
