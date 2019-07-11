878ce4593c699005cb02edbfd7109e7caa011338
嗝屁 <1010017806@qq.com>
2019-07-11 11:35:10 +0800

    HelloOperation

--- server/lib/controller/HelloOperation.js
+++ server/lib/controller/HelloOperation.js
@@ -16,6 +16,8 @@ class HelloOperation {
 
     async GetGoods(req, res, next) {
         try {
+
+             console.log("HelloOperation.          ...?GetGoods")
             let modules = req.query.modules;
             let current = req.query.current || 1;
             let pageSize = req.query.pageSize || 10;
