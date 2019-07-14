052a25eb83a39dd21c91b9300579f82820c05657
嗝屁 <1010017806@qq.com>
2019-07-14 17:38:17 +0800

    cc

--- src/manage/views/Goods/dataTable.vue
+++ src/manage/views/Goods/dataTable.vue
@@ -3,12 +3,12 @@
         <el-table align="center" v-loading="loading" ref="multipleTable" :data="dataList" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
             <el-table-column type="selection" width="55">
             </el-table-column>
-            <el-table-column prop="name" :label="$t('goods.price')" width="120">
+            <el-table-column prop="price" :label="$t('goods.price')" width="120">
             </el-table-column>
-            <el-table-column prop="comments" :label="$t('goods.days')">
+            <el-table-column prop="days" :label="$t('goods.days')">
             </el-table-column>
             </el-table-column>
-            <el-table-column prop="comments" :label="$t('goods.des')">
+            <el-table-column prop="des" :label="$t('goods.des')">
             </el-table-column>
             
             <el-table-column :label="$t('main.dataTableOptions')" width="150">
