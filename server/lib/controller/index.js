248d0a0f0b24a208b70fc7390d934cf55ccd627e
嗝屁 <1010017806@qq.com>
2019-07-21 16:59:35 +0800

    cc

--- server/lib/controller/index.js
+++ server/lib/controller/index.js
@@ -20,5 +20,5 @@ exports.SiteMessage = require('./siteMessage');
 //--------------------
 
 exports.HelloOperation = require('./HelloOperation');
-exports.PayUrl = require('./PayUrl');
+exports.PayUrl = require('./payUrl');
 
