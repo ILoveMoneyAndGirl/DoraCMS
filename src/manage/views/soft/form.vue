974cccb27162acd0b61b109df16f9b806986e023
嗝屁 <1010017806@qq.com>
2019-07-25 20:02:28 +0800

    c

--- src/manage/views/soft/form.vue
+++ src/manage/views/soft/form.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="dr-softForm">
-        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t(com.info)" :visible.sync="dialogState.show" :close-on-click-modal="false">
+        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('com.info')" :visible.sync="dialogState.show" :close-on-click-modal="false">
             <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
             
                 <el-form-item :label="$t('soft.name')" prop="name">
