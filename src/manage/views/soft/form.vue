845306710a055eb8bf155f4d9b50bd4a97d28779
嗝屁 <1010017806@qq.com>
2019-07-25 19:46:33 +0800

    cc

--- src/manage/views/soft/form.vue
+++ src/manage/views/soft/form.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="dr-softForm">
-        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('payUrl.form_title')" :visible.sync="dialogState.show" :close-on-click-modal="false">
+        <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t(产品信息)" :visible.sync="dialogState.show" :close-on-click-modal="false">
             <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
             
                 <el-form-item :label="$t('soft.name')" prop="name">
@@ -48,7 +48,7 @@ export default {
           name: [
           {
             required: true,
-            message: this.$t("validate.selectNull", {
+            message: this.$t("validate.inputNull", {
               label: this.$t("soft.name")
             }),
             trigger: "blur"
@@ -135,7 +135,7 @@ export default {
             });
           } else {
             // 新增
-            services.addPayUrl(params).then(result => {
+            services.addSoft(params).then(result => {
               if (result.data.status === 200) {
                 this.$store.dispatch("hideSoftForm");
                 this.$store.dispatch("getSoftList");
