c8a73f3306969c45e6ae29f106de2898f93c8d66
嗝屁 <1010017806@qq.com>
2019-07-31 17:00:04 +0800

    cc

--- src/manage/lang/zh.js
+++ src/manage/lang/zh.js
@@ -181,6 +181,11 @@ export default {
 
     getSoftArg:"获取参数",
     setSoftArg:"设置参数",
+
+    payProducttList:"支付产品列表",
+    payProductSoft:"添加支付产品",
+    updatePayProduct:"修改支付产品",
+    deletePayProduct:"删除支付产品",
     
   },
   navbar: {
--- src/manage/views/payProduct/urlForm.vue
+++ src/manage/views/payProduct/urlForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dr-payProductForm">
+    <div class="dr-payProductUrlForm">
         <el-dialog :xs="20" :sm="20" :md="6" :lg="6" :xl="6" size="small" :title="$t('com.info')" :visible.sync="dialogState.show" :close-on-click-modal="false">
             <el-form :model="dialogState.formData" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
                 <el-form-item :label="$t('payProduct.name')" prop="name">
