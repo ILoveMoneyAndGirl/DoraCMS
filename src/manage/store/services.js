a7ddb3c1f2dcc82715231fcc3e9c46f939c71d01
嗝屁 <1010017806@qq.com>
2019-09-09 01:32:59 +0800

    cc

--- src/manage/store/services.js
+++ src/manage/store/services.js
@@ -411,7 +411,7 @@ export default {
     },
 
     softSetArg(params) {
-        console.log("softSetArg------------>")
+        console.log("softSetArg------------>",params)
         return reqJsonData('manage/soft/setArg', params,'get')
     },
 
