f92a849c15c93c485585e318e52490eb8905d478
嗝屁 <1010017806@qq.com>
2019-09-09 08:49:20 +0800

    ccc

--- src/manage/views/proxy/form.vue
+++ src/manage/views/proxy/form.vue
@@ -57,7 +57,7 @@ export default {
           },
           {
             min: 1,
-            max: 12,
+            max: 102,
             message: this.$t("validate.rangelength", { min: 1, max: 102 }),
             trigger: "blur"
           }
@@ -72,8 +72,8 @@ export default {
           },
           {
             min: 1,
-            max: 10,  
-            message: this.$t("validate.rangelength", { min: 1, max: 12 }),
+            max: 102,  
+            message: this.$t("validate.rangelength", { min: 1, max: 102 }),
             trigger: "blur"
           }
         ],
