962e96fcc0e2069696c3d35e3c3140ad01eb91be
嗝屁 <1010017806@qq.com>
2019-09-15 19:19:39 +0800

    ccc

---
+++ src/manage/views/user/dataTable.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <el-table align="center" v-loading="loading" ref="multipleTable" :data="dataList" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+
+            <el-table-column prop="userName" :label="$t('user.userName')">
+            </el-table-column>
+            <el-table-column prop="password" :label="$t('user.password')">
+            </el-table-column>
+            <el-table-column prop="bornDate" :label="$t('user.bornDate')">
+            </el-table-column>
+          <el-table-column prop="deadLine" :label="$t('user.deadLine')">
+            </el-table-column>
+              <el-table-column prop="enable" :label="$t('user.enable')">
+            </el-table-column>
+
+        
+        </el-table>
+    </div>
+</template>
+
+<script>
+import services from "../../store/services.js";
+export default {
+  props: {
+    dataList: Array,
+    pageInfo: Object
+  },
+  data() {
+    return {
+      loading: false,
+      multipleSelection: []
+    };
+  },
+
+  methods: {
+
+  }
+};
+</script>
---
+++ src/manage/views/user/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="User">
+        <el-row class="dr-datatable">
+            <el-col :span="24">
+                <DataTable :pageInfo="UserList.pageInfo" :dataList="UserList.docs"></DataTable>
+                <Pagination :pageInfo="UserList.pageInfo" pageType="UserList"></Pagination>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+
+    import DataTable from './dataTable.vue';
+    import Pagination from '../common/Pagination.vue';
+    import {
+        mapGetters,
+        mapActions
+    } from 'vuex'
+
+    export default {
+        name: 'index',
+        data() {
+            return {
+
+            }
+        },
+        
+        components: {
+            DataTable,
+        },
+
+        methods: mapActions([
+
+        ]),
+        computed: {
+            ...mapGetters([
+                'UserList'
+            ]),
+        },
+        mounted() {
+            this.$store.dispatch('getUserList');
+        }
+    }
+</script>
+
+<style lang="">
+
+</style>
